bcf8554ee94eb105f8a5af03f3e2225703c44434
ThiagoMonts <thiagohonorato@hotmail.com>
2023-02-03 11:21:59 -0300

    Criação do Script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # word-scramble
  
+https://youtu.be/4-s3g_fU7Vg
---
+++ js/script.js
@@ -0,0 +1,15 @@
+const wordText = document.querySelector("")
+
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)] // getting random object from words
+    let wordArray = randomObj.word.split("") // splitting each letter of random word
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)) // getting a random number
+        //shuffling and swiping wordArray letters randomly
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
+        
+    }
+    console.log(wordArray, randomObj.word)
+}
+
+initGame()
