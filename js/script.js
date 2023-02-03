765ce6ce7cfaef870aca25f4834eb3b0f9015097
ThiagoMonts <thiagohonorato@hotmail.com>
2023-02-03 11:53:31 -0300

    Correção de bugs

--- js/script.js
+++ js/script.js
@@ -1,51 +1,47 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
-timeText = document.querySelector(".hint time"),
+timeText = document.querySelector(".time b"),
 inputField = document.querySelector("input"),
 refreshBtn = document.querySelector(".refresh-word"),
-checkBtn = document.querySelector(".check-word")
+checkBtn = document.querySelector(".check-word");
 
-let correctWord, timer
+let correctWord, timer;
 
 const initTimer = maxTime => {
-    clearInterval(timer)
+    clearInterval(timer);
     timer = setInterval(() => {
-        if (maxTime > 0) {
-            maxTime--
-            return timeText.innerText = maxTime
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
         }
-        clearInterval(timer)
-        alert(`Time off! ${correctWord.toUpperCase} was a correct word`)
-        initGame()
-    }, 1000)
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+        initGame();
+    }, 1000);
 }
 
 const initGame = () => {
-    initTimer(30)
-    let randomObj = words[Math.floor(Math.random() * words.length)] // getting random object from words
-    let wordArray = randomObj.word.split("") // splitting each letter of random word
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
     for (let i = wordArray.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1)) // getting a random number
-        //shuffling and swiping wordArray letters randomly
-        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
-        
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
-    wordText.innerText = wordArray.join("") // passing shuffled word as word text
-    hintText.innerText = randomObj.hint //passing random object hint as hint text
-    correctWord = randomObj.word.toLocaleLowerCase() // passing random word to correctWord
-    inputField.value = ""
-    inputField.setAttribute("maxlength", correctWord.length)
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
 }
-
-initGame()
+initGame();
 
 const checkWord = () => {
-    let userWord = inputField.value.toLocaleLowerCase() // getting user value
-    if (!userWord) return alert("Please enter a word check")
-    if (userWord !== correctWord) return alert(`Ops! ${userWord} is not a correct word`)
-    alert(`Congrats! ${userWord.toUpperCase} is a correct word`)
-    initGame()
+    let userWord = inputField.value.toLowerCase();
+    if(!userWord) return alert("Please enter the word to check!");
+    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
+    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
+    initGame();
 }
 
-refreshBtn.addEventListener("click", initGame)
-checkBtn.addEventListener("click", checkWord)
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
